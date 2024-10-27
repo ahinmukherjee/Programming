2d3094b2d64daabad88917763e2f538b3b579043
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-10-27 19:43:39 +0530

    while loop

---
+++ Loop/While_Loop/push.py
@@ -0,0 +1,13 @@
+
+n= int(input("please keyboard and press any value: "))
+for i in range(n):
+    print(1)
+
+for i in range(n):
+    print(1)
+
+for i in range(n):
+    print(1)
+    
+for i in range(n):
+    print(1)
