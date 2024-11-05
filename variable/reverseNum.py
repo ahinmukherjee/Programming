770873b9cfb3faaf4e545ef67df2428819021a14
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-11-05 19:34:04 +0530

    reverse number

--- variable/reverseNum.py
+++ variable/reverseNum.py
@@ -3,3 +3,9 @@ def number(n):
     return reversed_num
 num=12334
 print("number is",number(num))
+
+def number(n):
+    reversed_num = int(str(n))[::-1]
+    return reversed_num
+num=12334
+print("number is",number(num))
