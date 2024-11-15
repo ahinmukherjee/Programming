b5111cc2a300586199360521d17a0d6f805ce388
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-11-15 16:16:01 +0530

    c

---
+++ Loop/While_Loop/armstrong.java
@@ -0,0 +1,21 @@
+import java.util.*;
+  class armstrong{
+  public static void main(String args[]){
+      int r,n,c,arm=0;
+      Scanner sc= new Scanner(System.in);
+      n = sc.nextInt();
+      c=n;
+      while(n>0){
+      r= n%10;
+      arm=(r*r*r)+arm;
+      n=n/10;
+  }
+      if( c==arm){
+      System.out.println("number is armstrong");
+  }
+      else {
+      System.out.println("number is not armstrong");
+  }
+  sc.close();
+ }
+}
--- variable/degree.c
+++ variable/degree.c
@@ -1,9 +1,12 @@
-num=int(input("enter number"))
+#include<stdio.h>
+#include<conio.h>
+num=int(input("enter number"));
 sum1=0
 r=0
-while(num>0):
+while(num>0){
     r=num%10
     sum1=sum1+r
     num=num//10
     print("Addition of digit for every step", sum1)
-print(sum1)
+}
+print(sum1);
