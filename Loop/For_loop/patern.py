9ba7ecbb0ef3001e23cae7ecdd9e0221b5561714
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-02-06 18:39:23 +0530

    editting

--- Loop/For_loop/patern.py
+++ Loop/For_loop/patern.py
@@ -1,11 +1,11 @@
 # *
-# **
-# ***
-# ****
-# *****
+# * *
+# * * *
+# * * * *
+# * * * * *
 
 n="*"
 for i in range(5):
     for j in range(i+1):
-        print("*", end="")
+        print("*",'', end="")
     print()
