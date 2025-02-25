632602ec247f645ba85e13ec866076dadffb6e5d
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-02-25 21:37:04 +0530

    edit file

--- Array/ArrayIndexFind.java
+++ Array/ArrayIndexFind.java
@@ -1,18 +1,23 @@
+
 import java.util.*;
-class ArrayIndexFind{
-  public static void main(String args[]){
-     Scanner sc= new Scanner(System.in);
-     System.out.println("Enter elements that you want");
-     int n= sc.nextInt();
-     int arr[]= new int[n];
-     System.out.println("Enter value");
-     for(int i=0; i<=n; i++){
-	arr[i]= sc.nextInt();
-     }
-     System.out.println("Value is bellow");
-     for(int i=0; i<=n; i++){
-	System.out.println(arr[i]);
-     }
-     sc.close();	
-  }
+
+class ArrayIndexFind {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter values:");
+        for (int i = 0; i <n; i++) {  
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Values are below:");
+        for (int i = 0; i <n; i++) {  
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
 }
