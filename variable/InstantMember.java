ead691057a4dd91e3ff631f4d77f6ba46de50cc8
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-03-04 21:22:46 +0530

    files add

---
+++ variable/InstantMember.java
@@ -0,0 +1,8 @@
+public class InstantMember {
+        int  b=10;
+        public static void main(String args[]){
+           InstantMember s1= new InstantMember();
+            System.out.println(s1.b);
+        }
+    }
+    
