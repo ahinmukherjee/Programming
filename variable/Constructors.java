ec1e588b808bf9e7a4d96cb7477b9def194aaf9d
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-03-07 21:35:51 +0530

    Change file

---
+++ variable/Constructors.java
@@ -0,0 +1,41 @@
+// import java.util.Scanner;
+
+// class A{
+//     int n,r;
+//     // int count=0;
+//     // a=0;
+//     // n=c;
+//     // int sum=0;
+//     while(n>0){
+//         n=n/10;
+//         count++;
+//     }
+//     System.out.println("Numbers digits is"+count);
+//     if(count==3){
+//         while(n>0){
+//             r = n %10;
+//             sum = sum*10+r;
+//             n=n/10;
+//             // System.out.println("Reverse number is"+sum);
+//         }
+//     System.out.println("Reverse number is" +sum);        
+//     }
+    
+//     else{
+//         System.out.println("please enter 3 digits value");
+//     } 
+//     sc.close();
+// }
+
+// public class Constructors {
+    
+//     public static void main(String args[]){
+//      Scanner sc= new Scanner(System.in);
+//         System.out.println("enetr number:");
+//         // n=sc.nextInt();
+//         // while(n>0){
+//             // n=n/10;
+//             // count++;
+//         }
+//         // System.out.println("Numbers digits is"+count);
+// }
