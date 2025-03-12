c7363137dd5f3370c75069b907530f6aaf3fbeb7
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-03-12 20:35:34 +0530

    Constructor

--- variable/Constructors.java
+++ variable/Constructors.java
@@ -1,41 +1,17 @@
-// import java.util.Scanner;
+class A{
+    A (int x){
+        int s= x * x;
+        System.out.println("Multiplication ="+s);
+   }
+    A (int x, int y){
+     int s= x + y;
+     System.out.println("Sum ="+s);
+   }
+}
+class ConstructorOverloading {
+    public static void main(String args[]){
+        new A( 3 );
+        new A( 3,2 );
+    }
+}
 
-// class A{
-//     int n,r;
-//     // int count=0;
-//     // a=0;
-//     // n=c;
-//     // int sum=0;
-//     while(n>0){
-//         n=n/10;
-//         count++;
-//     }
-//     System.out.println("Numbers digits is"+count);
-//     if(count==3){
-//         while(n>0){
-//             r = n %10;
-//             sum = sum*10+r;
-//             n=n/10;
-//             // System.out.println("Reverse number is"+sum);
-//         }
-//     System.out.println("Reverse number is" +sum);        
-//     }
-    
-//     else{
-//         System.out.println("please enter 3 digits value");
-//     } 
-//     sc.close();
-// }
-
-// public class Constructors {
-    
-//     public static void main(String args[]){
-//      Scanner sc= new Scanner(System.in);
-//         System.out.println("enetr number:");
-//         // n=sc.nextInt();
-//         // while(n>0){
-//             // n=n/10;
-//             // count++;
-//         }
-//         // System.out.println("Numbers digits is"+count);
-// }
