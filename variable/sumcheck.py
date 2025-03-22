6b71acc1b80fbcecc9a1f072351b8e8a9f4d17e3
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-03-22 21:40:02 +0530

    create

--- variable/sumcheck.py
+++ variable/sumcheck.py
@@ -1,15 +1,11 @@
-l=[7, 1, 2, 3, 4, 5, 8]
-s=0
-for i  in range(len(l)):
-    for j in range(i+1, len(l)):
-        if(l[i]>l[j]):
-            s= l[i]-l[j]
-        print("values of",l[i],"and",l[j],"is",s ) 
-        # elif(l[i]>l[j]):
-        #     s= l[i]-l[j]
-        # print("values of",l[i],"and",l[j],"is",s )
-
-              
-            
-    j+=1
-
+l = [7, 1, 2, 3, 4, 5, 8]
+s = 0
+p=0
+for i in range(len(l)):
+    for j in range(i + 1, len(l)):
+        if (l[j] > l[i]):
+            s = l[j] - l[i]
+            print("Values of", l[j], "and", l[i], "is=", s)
+        elif (l[j] < l[i]):
+            p = l[j] - l[i]
+            print("Values of", l[j], "and", l[i], "is", p)
