4ceb1d344eee1e30732625da44762a7783eb210a
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-03-24 21:37:11 +0530

    queue

---
+++ Queue/queue.py
@@ -0,0 +1,46 @@
+class node:
+    def __init__(self):
+        self.data= int(input("Data is"))
+        self.next=None
+
+class Queue:
+    def create(self):
+        global front,rear
+        temp=node()
+        if(rear==None):
+            front=temp
+            rear=temp
+        else:
+            rear.next=temp
+            rear=temp
+
+    def delete(self):
+        if(front==None):
+            print("Queue is empty")
+        else:
+            temp=front
+            print("Dleteed data is", temp.data)
+            front=front.next
+            del(temp)
+
+    def display(self):
+        temp=rear
+        while(temp!=None):
+            print(temp.data)
+            temp=temp.next
+
+
+front=rear=None
+a=Queue()
+while True:
+    print(1: "Create")
+    print(2:"Delete")
+    print(3:"Display")
+    
+ch=input("your choice")
+if ch==1:
+    a.create()
+elif ch==2:
+   a.delete()
+elif ch==3:
+   a.display()
--- variable/sumcheck.py
+++ variable/sumcheck.py
@@ -1,6 +1,5 @@
 l = [7, 1, 2, 3, 4, 5, 8]
-s = 0
-p=0
+
 for i in range(len(l)):
     for j in range(i + 1, len(l)):
         if (l[j] > l[i]):
@@ -9,3 +8,5 @@ for i in range(len(l)):
         elif (l[j] < l[i]):
             p = l[j] - l[i]
             print("Values of", l[j], "and", l[i], "is", p)
+        elif(s<p):
+            print("hello")
