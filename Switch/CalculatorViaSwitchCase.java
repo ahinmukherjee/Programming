a59bf5fb6938e6c67f5ef66aa4724225b64ba824
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-05-03 21:39:01 +0530

    exception solved

--- Switch/CalculatorViaSwitchCase.java
+++ Switch/CalculatorViaSwitchCase.java
@@ -1,74 +1,68 @@
-// package Switch;
+import java.util.*;
 
-import java.util.Scanner;
-class CalculatorTests {
-     void add(int a, int b){
-		int sum= a+b;
+class CalculatorTest {
+     void add(double a, double b){
+		double sum= a+b;
 		System.out.println("Addition is:" +sum);	
 	}
-
-	void subs(int a, int b){
-		int sub= a-b;
+	void subs(double a, double b){
+		double sub= a-b;
 		System.out.println("Subtraction is:" +sub);	
 	}
-	void multi(int a, int b){
-		int mul= a*b;
+	void multi(double a, double b){
+		double mul= a*b;
 		System.out.println("Multiplication is:" +mul);	
 	}
-	void divi(int a, int b){
-		int vag= a/b;
-		System.out.println("Division is:" +vag);	
+	void divi(double a, double b){
+		double vag= a/b;
+		System.out.println("Divition is:" +vag);	
 	}	
 }
 
-public class CalculatorViaSwitchCase {
+public class CalculatorViaSwitchCase{
     public static void main(String args[]){
-		Scanner sc= new Scanner(System.in);
-		CalculatorTests rn= new CalculatorTests();
+	Scanner sc= new Scanner(System.in);
+	CalculatorTest rn= new CalculatorTest();
 		try{
-			try{
-			
-				System.out.println("Enter first number");
-				int num1 = sc.nextInt();
-				System.out.println("Enter second number");
-				int num2 = sc.nextInt();
-			
-				System.out.println("Enter 1 for Addition || Enter 2 for Subtraction || Enter 3 for Multiplication || Enter 4 for Divition|| Enter 5 for Exit");
-				int choices=sc.nextInt();
-			}
+		
+			System.out.println("Enter first number");
+			double num1 = sc.nextDouble();
+			System.out.println("Enter second number");
+			double num2 = sc.nextDouble();
+			System.out.println("Enter 1 for Addition || Enter 2 for Subtraction || Enter 3 for Multiplication || Enter 4 for Divition|| Enter 5 for Exit");
+			int choices=sc.nextInt();
 			
 			switch(choices){
-			case 1: 
-				rn.add(num1, num2);
-				break;
-			case 2: 
-				rn.subs(num1, num2);
-				break;
-			case 3: 
-				rn.multi(num1, num2);
-				break;
-			case 4: 
-				rn.divi(num1, num2);
-				break;
-			case 5: 
-				System.exit(0);
-			default:
-				System.out.println("Plz enter the screen numbers");
-			}
-			finally{
-				sc.close(); 
-			}
-		
+				case 1: 
+					rn.add(num1, num2);
+					break;
+				case 2: 
+					rn.subs(num1, num2);
+					break;
+				case 3: 
+					rn.multi(num1, num2);
+					break;
+				case 4: 
+					rn.divi(num1, num2);
+					break;
+				case 5: 
+					System.exit(0);
+				default:
+					System.out.println("Plz enter the screen numbers");
+		    }
+        }
+		catch(ArithmeticException ae){
+			System.out.println("Doesn't divisibel by zero: "+ae);;
 		}
-		catch(ArithemeticException ae){
-			System.out.println("Doesn't divisibel by zero"+ae);;
-		}
-		catch(InputMissmatchException im){
-			System.out.println("Plz enter valid inters number"+im);
+		catch(InputMismatchException ime){
+			System.out.println("Doesn't match valid input numbers: "+ime);
 		}
 		catch(Exception e){
 			System.out.println("Doesn't match input numbers"+e);
 		}
-		 
-	}
+		finally{
+			sc.close();
+		}
+	 
+    }
 }
